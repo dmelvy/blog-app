f225666ce598266061b99f442dabb204cb1605d4
Pat Ray <patraydev@gmail.com>
2021-03-03 13:54:40 -0500

    something showed up!

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,13 +1,15 @@
 import './Home.css';
 import PostCards from '../../components/PostCards/PostCards';
-import Layout from '../../components/shared/Layout/Layout'
+import Layout from '../../shared/Layout/Layout.jsx';
 
 const Home = () => {
-  <Layout>
-    <div className='home'>
-      <PostCards />
-    </div>
-  </Layout>
-};
+  return (
+    <Layout>
+      <div className='home'>
+        <PostCards />
+      </div>
+    </Layout>
+  )
+}
 
 export default Home
