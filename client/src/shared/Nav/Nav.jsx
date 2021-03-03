ec13542e06af35f55560e078ad02b2a7cfff1f7c
Pat Ray <patraydev@gmail.com>
2021-03-03 15:29:10 -0500

    form component changes

--- client/src/shared/Nav/Nav.jsx
+++ client/src/shared/Nav/Nav.jsx
@@ -1,15 +1,19 @@
 import './Nav.css'
 import { NavLink } from 'react-router-dom';
+import logo from './logo192.png';
 
 function Nav(props) {
   return (
+    <>
+      <img className='logo-image' src={logo} alt='our badass logo'/>
     <div className='nav'>
-      <NavLink className='logo' to='/'>App Name!</NavLink>
+      <NavLink className='logo-text' to='/'>CRUD BLOG</NavLink>
       <div className='nav-links'>
         <NavLink className='nav-link' to='/posts'>Posts</NavLink>
         <NavLink className='nav-link' to='/add-post'>Add Post</NavLink>
         </div>
-    </div>
+      </div>
+      </>
   )
 }
 
