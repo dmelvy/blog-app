2213a45eebc319e2c1e974d72037226ef1082f19
Jessica Embury <juannajane@Jessicas-MacBook-Air.local>
2021-03-03 08:48:32 -0800

    add floppy pics

--- seed/posts.js
+++ seed/posts.js
@@ -29,6 +29,12 @@ const main = async () => {
         "imgURL": "https://images.unsplash.com/photo-1523812483161-8af21dad90dc?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NTB8fHNpbGx5JTIwdHJpbmtldCUyMGNvbGxlY3Rpb258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
         "content": "Check out my estate",
         "author" : "grandma"
+      },
+      {
+        "title": "Never to be Forgotten",
+        "imgURL": "https://images.unsplash.com/photo-1536604673810-81370412626d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NTV8fHNpbGx5JTIwdHJpbmtldCUyMGNvbGxlY3Rpb258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+        "content": "what used to be is now just memories past...",
+        "author" : "RIPFloppyDisk"
     },
     ]
   
